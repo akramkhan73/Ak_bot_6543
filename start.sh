6122f9d52b1dd00322b6802d4d78c264a76f8a53
akramkhan73 <145324243+akramkhan73@users.noreply.github.com>
2023-09-22 08:56:50 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
+git clone https://github.com/akramkhan73/Ak_bot_6543 /Ak_bot_6543 boted;
 cd boted;
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
